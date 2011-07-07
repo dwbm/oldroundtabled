37297404a078cc8a2aa60d452957dc89337b55a2
Daniel WB Mazzone <dwbm@danielwbmazzone.com>
2011-07-07 10:48:23 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roundtabled::Application.config.secret_token = 'ddfe620001c3aaba96367060a80b264b683ec5bf34a4809a9c49bf76d1fe927d6e59d42031b377ada3f4f7f875ca32531553072b07ef3836b2149fe73c8fc3ed'
